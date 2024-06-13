0fe111b3fff9eebdea97eb682d4d7dfc15a3c86c
Pablo Alan <pabloxt14@gmail.com>
2024-06-13 20:00:00 -0300

    docs: start clean code studies in react

---
+++ codigo-limpo-no-react/src/app.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Click me
+      </button>
+    </>
+  )
+}
