3f571fa55fd0e9017d3d2869794b23c22cc2f494
Pablo Alan <pabloxt14@gmail.com>
2024-06-13 20:30:00 -0300

    docs: forget detail

--- codigo-limpo-no-react/src/app.tsx
+++ codigo-limpo-no-react/src/app.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
     <>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      <button className="bg-sky-700 px-4 sm:py-3 py-2 text-white hover:bg-sky-800 sm:px-8">
         Click me
       </button>
     </>
