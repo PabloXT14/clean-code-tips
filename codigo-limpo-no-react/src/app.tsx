99222863c4b03d854cefe3cca877609cc985062e
Pablo Alan <pabloxt14@gmail.com>
2024-06-17 20:00:00 -0300

    docs: continuing implemente base project structure

--- codigo-limpo-no-react/src/app.tsx
+++ codigo-limpo-no-react/src/app.tsx
@@ -5,6 +5,7 @@ import {
   LuFileCheck,
   LuFileX,
   LuRefreshCcw,
+  LuCheckCircle2,
 } from 'react-icons/lu'
 
 export function App() {
@@ -36,8 +37,8 @@ export function App() {
   // const currentYear = new Date().getFullYear()
 
   return (
-    <div className="min-h-screen w-full pt-16">
-      <div className="mx-auto max-w-[450px]">
+    <div className="h-screen w-full overflow-hidden pt-16">
+      <div className="mx-auto h-full max-w-[450px]">
         <header className="mb-8 flex w-full items-center justify-between">
           <div className="flex items-center gap-4">
             <img
@@ -73,7 +74,7 @@ export function App() {
 
               <div className="flex flex-col justify-center">
                 <h3 className="text-lg font-semibold">On going</h3>
-                <span className="text-xs text-zinc-700">24 Tasks</span>
+                <span className="text-xs text-zinc-50">24 Tasks</span>
               </div>
             </div>
 
@@ -85,7 +86,7 @@ export function App() {
 
               <div className="flex flex-col justify-center">
                 <h3 className="text-lg font-semibold">In process</h3>
-                <span className="text-xs text-zinc-700">12 Tasks</span>
+                <span className="text-xs text-zinc-50">12 Tasks</span>
               </div>
             </div>
 
@@ -97,7 +98,7 @@ export function App() {
 
               <div className="flex flex-col justify-center">
                 <h3 className="text-lg font-semibold">Completed</h3>
-                <span className="text-xs text-zinc-700">42 Tasks</span>
+                <span className="text-xs text-zinc-50">42 Tasks</span>
               </div>
             </div>
 
@@ -109,26 +110,76 @@ export function App() {
 
               <div className="flex flex-col justify-center">
                 <h3 className="text-lg font-semibold">Canceled</h3>
-                <span className="text-xs text-zinc-700">8 Tasks</span>
+                <span className="text-xs text-zinc-50">8 Tasks</span>
               </div>
             </div>
           </section>
 
-          <h2>Advantages</h2>
+          <h2 className="mb-3 text-xl font-semibold">Recent Tasks</h2>
 
-          <section>
-            <div>
-              <h3>Blazing fast</h3>
-              <p>This to-do list app is insanely fast.</p>
-            </div>
+          <ul className="space-y-4 overflow-y-auto">
+            <li>
+              {/* Task Card */}
+              <div className="relative flex items-center rounded-2xl border border-zinc-900 bg-white p-4 pr-5">
+                {/* Left side */}
+                <div className="flex-1">
+                  <h3 className="text-lg font-semibold">Website for Rune.io</h3>
+                  <span className="mb-2 block text-xs text-zinc-400">
+                    Digital Product Design
+                  </span>
+                  <div className="flex items-center gap-1">
+                    <LuCheckCircle2 className="size-4" />
+                    <span className="text-xs font-medium">12 Tasks</span>
+                  </div>
+                </div>
 
-            <div>
-              <h3>Easy to use</h3>
-              <p>You can add and remove todos in seconds.</p>
-            </div>
-          </section>
+                {/* Right side */}
+                <div>
+                  {/* Progress Bar Container */}
+                  <div className="relative size-16">
+                    {/* mesmo valor em rem no stroke do circle */}
+                    {/* Outer */}
+                    <div className="size-16 rounded-full border-[0.4rem] border-zinc-200">
+                      {/* Inner */}
+                      <div className="flex h-full w-full items-center justify-center">
+                        {/* Number */}
+                        <div className="text-sm font-semibold text-zinc-900">
+                          40%
+                        </div>
+                      </div>
+                    </div>
 
-          <ul>
+                    {/* SVG Progress Bar */}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      version="1.1"
+                      width="100%"
+                      height="100%"
+                      className="absolute left-0 top-0 -rotate-90"
+                    >
+                      <defs>
+                        <linearGradient id="GradientColor">
+                          <stop offset="0%" stopColor="#FEC347" />
+                          <stop offset="100%" stopColor="#F26E56" />
+                        </linearGradient>
+                      </defs>
+                      <circle
+                        cx="50%"
+                        cy="50%"
+                        r="43.85%"
+                        strokeLinecap="round"
+                        strokeDasharray={200}
+                        strokeDashoffset={200 - (200 * 40) / 100}
+                        className="fill-none stroke-rose-400 stroke-[0.4rem]"
+                      />
+                    </svg>
+                  </div>
+                </div>
+
+                {/* Bottom Border */}
+                <div className="absolute left-1/2 top-1.5 z-[-1] h-full w-[98.5%] -translate-x-1/2 rounded-3xl bg-zinc-900" />
+              </div>
+            </li>
             {todos.map((todo) => (
               <li key={todo.id}>{todo.text}</li>
             ))}
