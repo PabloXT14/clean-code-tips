5193b5f411168779c3deb2c6ad0b4bdc41e3ddbd
Pablo Alan <pabloxt14@gmail.com>
2024-06-18 20:00:00 -0300

    docs: almost finishing tasks list layout

--- codigo-limpo-no-react/src/app.tsx
+++ codigo-limpo-no-react/src/app.tsx
@@ -7,39 +7,62 @@ import {
   LuRefreshCcw,
   LuCheckCircle2,
 } from 'react-icons/lu'
+import { twMerge } from 'tailwind-merge'
+
+type Todo = {
+  id: number
+  title: string
+  description: string
+  totalTasks: number
+  completedTasks: number
+  status: 'canceled' | 'completed' | 'in progress' | 'on going'
+}
 
 export function App() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [todos, setTodos] = useState([
+  const [todos, setTodos] = useState<Todo[]>([
     {
       id: 1,
-      text: 'Learn React',
-      status: 'active',
+      title: 'Website for Rune.io',
+      description: 'Digital Product Design',
+      totalTasks: 12,
+      completedTasks: 5,
+      status: 'canceled',
     },
     {
       id: 2,
-      text: 'Learn TypeScript',
-      status: 'active',
+      title: 'Dashboard for ProSavvy',
+      description: 'Digital Product Design',
+      totalTasks: 12,
+      completedTasks: 9,
+      status: 'completed',
     },
     {
       id: 3,
-      text: 'Learn Tailwind',
-      status: 'active',
+      title: 'Mobile Apps for Track.id',
+      description: 'Digital Product Design',
+      totalTasks: 12,
+      completedTasks: 6,
+      status: 'in progress',
     },
-
     {
       id: 4,
-      text: 'Learn React Native',
-      status: 'active',
+      title: 'Website for CourierGo.com',
+      description: 'Digital Product Design',
+      totalTasks: 12,
+      completedTasks: 5,
+      status: 'on going',
     },
   ])
 
-  // const currentYear = new Date().getFullYear()
+  function calculatePercentage(completedTasks: number, totalTasks: number) {
+    return Math.round((completedTasks / totalTasks) * 100)
+  }
 
   return (
-    <div className="h-screen w-full overflow-hidden pt-16">
-      <div className="mx-auto h-full max-w-[450px]">
-        <header className="mb-8 flex w-full items-center justify-between">
+    <div className="flex h-screen w-full flex-col overflow-hidden border">
+      <div className="mx-auto flex h-full max-w-[450px] flex-col">
+        <header className="my-8 flex w-full items-center justify-between">
           <div className="flex items-center gap-4">
             <img
               src="https://randomuser.me/api/portraits/men/18.jpg"
@@ -64,7 +87,8 @@ export function App() {
           </button>
         </header>
 
-        <main>
+        <main className="flex flex-1 flex-col overflow-hidden">
+          {/* Stats */}
           <section className="mb-8 grid grid-cols-2 gap-4">
             {/* On going */}
             <div className="flex items-center gap-3 rounded-2xl bg-blue-400 p-4">
@@ -117,76 +141,98 @@ export function App() {
 
           <h2 className="mb-3 text-xl font-semibold">Recent Tasks</h2>
 
-          <ul className="space-y-4 overflow-y-auto">
-            <li>
-              {/* Task Card */}
-              <div className="relative flex items-center rounded-2xl border border-zinc-900 bg-white p-4 pr-5">
-                {/* Left side */}
-                <div className="flex-1">
-                  <h3 className="text-lg font-semibold">Website for Rune.io</h3>
-                  <span className="mb-2 block text-xs text-zinc-400">
-                    Digital Product Design
-                  </span>
-                  <div className="flex items-center gap-1">
-                    <LuCheckCircle2 className="size-4" />
-                    <span className="text-xs font-medium">12 Tasks</span>
-                  </div>
-                </div>
-
-                {/* Right side */}
-                <div>
-                  {/* Progress Bar Container */}
-                  <div className="relative size-16">
-                    {/* mesmo valor em rem no stroke do circle */}
-                    {/* Outer */}
-                    <div className="size-16 rounded-full border-[0.4rem] border-zinc-200">
-                      {/* Inner */}
-                      <div className="flex h-full w-full items-center justify-center">
-                        {/* Number */}
-                        <div className="text-sm font-semibold text-zinc-900">
-                          40%
-                        </div>
+          {/* Tasks List */}
+          <ul className="flex-1 space-y-4 overflow-y-auto">
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {/* Task Card */}
+                <div className="relative">
+                  <div className="relative z-10 flex items-center rounded-2xl border border-zinc-900 bg-white p-4 pr-5">
+                    {/* Left side */}
+                    <div className="flex-1">
+                      <h3 className="text-lg font-semibold">{todo.title}</h3>
+                      <span className="mb-2 block text-xs text-zinc-400">
+                        {todo.description}
+                      </span>
+                      <div className="flex items-center gap-1">
+                        <LuCheckCircle2 className="size-4" />
+                        <span className="text-xs font-medium">
+                          {todo.totalTasks} Tasks
+                        </span>
                       </div>
                     </div>
 
-                    {/* SVG Progress Bar */}
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      version="1.1"
-                      width="100%"
-                      height="100%"
-                      className="absolute left-0 top-0 -rotate-90"
-                    >
-                      <defs>
-                        <linearGradient id="GradientColor">
-                          <stop offset="0%" stopColor="#FEC347" />
-                          <stop offset="100%" stopColor="#F26E56" />
-                        </linearGradient>
-                      </defs>
-                      <circle
-                        cx="50%"
-                        cy="50%"
-                        r="43.85%"
-                        strokeLinecap="round"
-                        strokeDasharray={200}
-                        strokeDashoffset={200 - (200 * 40) / 100}
-                        className="fill-none stroke-rose-400 stroke-[0.4rem]"
-                      />
-                    </svg>
-                  </div>
-                </div>
+                    {/* Right side */}
+                    <div>
+                      {/* Progress Bar Container */}
+                      <div className="relative size-16">
+                        {/* mesmo valor em rem no stroke do circle */}
+                        {/* Outer */}
+                        <div className="size-16 rounded-full border-[0.4rem] border-zinc-200">
+                          {/* Inner */}
+                          <div className="flex h-full w-full items-center justify-center">
+                            {/* Number */}
+                            <div className="text-sm font-semibold text-zinc-900">
+                              {calculatePercentage(
+                                todo.completedTasks,
+                                todo.totalTasks,
+                              )}
+                              %
+                            </div>
+                          </div>
+                        </div>
 
-                {/* Bottom Border */}
-                <div className="absolute left-1/2 top-1.5 z-[-1] h-full w-[98.5%] -translate-x-1/2 rounded-3xl bg-zinc-900" />
-              </div>
-            </li>
-            {todos.map((todo) => (
-              <li key={todo.id}>{todo.text}</li>
+                        {/* SVG Progress Bar */}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          version="1.1"
+                          width="100%"
+                          height="100%"
+                          className="absolute left-0 top-0 -rotate-90"
+                        >
+                          <defs>
+                            <linearGradient id="GradientColor">
+                              <stop offset="0%" stopColor="#FEC347" />
+                              <stop offset="100%" stopColor="#F26E56" />
+                            </linearGradient>
+                          </defs>
+                          <circle
+                            cx="50%"
+                            cy="50%"
+                            r="43.85%"
+                            strokeLinecap="round"
+                            strokeDasharray={200}
+                            strokeDashoffset={
+                              230 -
+                              (230 *
+                                calculatePercentage(
+                                  todo.completedTasks,
+                                  todo.totalTasks,
+                                )) /
+                                100
+                            }
+                            className={twMerge(
+                              'fill-none stroke-[0.4rem]',
+                              todo.status === 'on going' && 'stroke-blue-400',
+                              todo.status === 'in progress' &&
+                                'stroke-amber-400',
+                              todo.status === 'completed' && 'stroke-teal-400',
+                              todo.status === 'canceled' && 'stroke-rose-400',
+                            )}
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Bottom Border */}
+                  <div className="absolute left-1/2 top-1 z-0 h-full w-[96.5%] -translate-x-1/2 rounded-2xl bg-zinc-900" />
+                </div>
+              </li>
             ))}
           </ul>
         </main>
 
-        <footer></footer>
+        <footer className="flex items-center justify-center py-2">Teste</footer>
       </div>
     </div>
   )
